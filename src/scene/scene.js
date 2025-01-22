2cd0fbf13252e8276937a78e324c54a6f38cda0f
Chahat Kalsi <chahat.ck88@gmail.com>
2025-01-22 09:37:11 +0530

    projection matrix added

--- src/scene/scene.js
+++ src/scene/scene.js
@@ -5,6 +5,7 @@ import { Camera } from './camera';
 import { getShapeProgram } from '../shaders/shaders.js';
 import vertexShaderSource from '../shaders/shaders/vertexShaderSource.vert';
 import fragmentShaderSource from '../shaders/shaders/fragmentShaderSource.frag';
+import { mat4 } from 'gl-matrix';
 
 export class Scene {
     constructor(gl) {
@@ -12,6 +13,7 @@ export class Scene {
 
         this.program = getShapeProgram(gl, vertexShaderSource, fragmentShaderSource);
         this.objects = this.setupObjects();
+        this.setupProjectionMatrix();
 
         this.clearColor = [0.988, 0.796, 0, 1.0];
 
@@ -28,5 +30,13 @@ export class Scene {
         objects.push(new Triangle(this.gl, this.program, 0.7, [1.0,0.0,0.0,1.0]));
         objects.push(new Square(this.gl, this.program, 0.5, [0.0, 0.0, 1.0, 1.0]));
         return objects;
+    }
+
+    setupProjectionMatrix() {
+        const projectionMatrix = mat4.create();
+        mat4.perspective(projectionMatrix, Math.PI / 4.0, 800 / 600, 0.1, 1000.0);
+        this.gl.useProgram(this.program);
+        const uProjMatrixLoc = this.gl.getUniformLocation(this.program, 'uProjectionMatrix');
+        this.gl.uniformMatrix4fv(uProjMatrixLoc, false, projectionMatrix);
     }
 }
